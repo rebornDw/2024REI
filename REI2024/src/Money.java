5c62a054c8fb7b93d1c4ea51e96e8288d1c6a863
liu12345611 <l2655519642@gmail.com>
2024-11-19 17:09:30 +0800

    第二次提交

---
+++ REI2024/src/Money.java
@@ -0,0 +1,15 @@
+
+public class Money {
+	public static void main(String[] args) {
+		Shop xinsu=new Shop();
+		xinsu.name = "好吃";
+		xinsu.weiZhi="新宿";
+		xinsu.jinE=10000;
+		xinsu.yingYee();
+		System.out.println(xinsu.name);
+		System.out.println(xinsu.weiZhi);
+		System.out.println(xinsu.jinE);
+		
+	}
+
+}
