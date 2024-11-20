7c092d7e246f7f5580b25db9586f49b06dced447
liu12345611 <l2655519642@gmail.com>
2024-11-20 18:03:49 +0800

    二次提交

---
+++ src/Money.java
@@ -0,0 +1,25 @@
+
+public class Money {
+	public static void main(String[] args) {
+	
+
+	Shop xinsu=new Shop();
+	xinsu.yingYee();
+	xinsu.name="新宿";
+	xinsu.weiZhi="新宿区";
+	xinsu.jinE= (1000000);
+	System.out.println(xinsu.name);
+	System.out.println(xinsu.weiZhi);
+	System.out.println(xinsu.jinE);
+	
+	Shop zhongye =new Shop();
+	zhongye.name="中野";
+	zhongye.weiZhi="中野区";
+	zhongye.jinE=(20000);
+	System.out.println(zhongye.name);
+	System.out.println(zhongye.weiZhi);
+	System.out.println(zhongye.jinE);
+
+}
+	
+}
---
+++ src/Shop.java
@@ -0,0 +1,13 @@
+
+public class Shop {
+	public String name;
+	public String weiZhi;
+	public int  jinE;
+	
+	
+	public void yingYee() {
+		System.out.println("营业额");
+	}
+
+
+}
