a6e2262f0d1f553c37cd2c6855cfc6fc603b0164
fqsic <75964723+fqsic@users.noreply.github.com>
2024-11-25 01:25:09 +0800

    Delete java01 directory

--- java01/src/test02/Book.java
+++
@@ -1,8 +0,0 @@
-package test02;
-
-public class Book {
-
-	public String bookName;
-	public String bookAuthor;
-	public int price;
-}
--- java01/src/test04/UserInfo.java
+++
@@ -1,9 +0,0 @@
-package test04;
-
-public class UserInfo {
-	// 用户名
-	public String userName;
-	// 用户密码
-	public String userPassWord;
-
-}
